2df62f0443267b2640eab7f3d1b64b4bd1b697d1
Brandon Flores <bsflores@wisc.edu>
2022-04-06 12:29:39 -0500

    Made factors of π optional (enabled by default) for the printing of `Spherical` and `Polar`

--- src/show.jl
+++ src/show.jl
@@ -1,14 +1,23 @@
-function Base.show(io::IO, ::MIME"text/plain", v::Polar)
+function Base.show(io::IO, ::MIME"text/plain", v::Polar; usepi=true)
     println(io, typeof(v), ":")
     println(io, " r: ", r(v))
     # TODO: do I want to have the factor of π included?
-    print(io, " θ: ", theta(v)/π, "π")
+    if usepi
+        print(io, " θ: ", theta(v)/π, "π")
+    else
+        print(io, " θ: ", theta(v))
+    end
 end
 
-function Base.show(io::IO, ::MIME"text/plain", v::Spherical)
+function Base.show(io::IO, ::MIME"text/plain", v::Spherical; usepi=true)
     println(io, typeof(v), ":")
     println(io, " r: ", r(v))
     # TODO: do I want to have the factor of π included?
-    println(io, " θ: ", theta(v)/π, "π")
-    print(io, " ϕ: ", phi(v)/π, "π")
+    if usepi
+        println(io, " θ: ", theta(v)/π, "π")
+        print(io, " ϕ: ", phi(v)/π, "π")
+    else
+        println(io, " θ: ", theta(v))
+        print(io, " ϕ: ", phi(v))
+    end
 end
