8de1cbe2bffda74451b650a42e1d0dbdc11c40e8
Brandon Flores <bsflores@wisc.edu>
2022-04-06 12:26:27 -0500

    Added pretty printing for `Polar`

--- src/show.jl
+++ src/show.jl
@@ -1,3 +1,10 @@
+function Base.show(io::IO, ::MIME"text/plain", v::Polar)
+    println(io, typeof(v), ":")
+    println(io, " r: ", r(v))
+    # TODO: do I want to have the factor of π included?
+    print(io, " θ: ", theta(v)/π, "π")
+end
+
 function Base.show(io::IO, ::MIME"text/plain", v::Spherical)
     println(io, typeof(v), ":")
     println(io, " r: ", r(v))
